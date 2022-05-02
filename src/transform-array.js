a8b5b90a54a6edeeb9ca433aba1aa7520c4c626d
Nikolay <kolyak770@gmail.com>
2022-05-02 17:20:27 +0300

    resolve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -19,16 +19,22 @@ function transform(arr) {
   for(let i = 0; i < copyArr.length; i++) {
     switch (copyArr[i]) {
       case '--discard-next':
-        copyArr[i+1] ? copyArr.splice(i, 2) : copyArr.splice(i, 1);
-        i--;
-        break;
+          if (copyArr[i + 2] === '--double-prev' || copyArr[i + 2] === '--discard-prev') {
+            copyArr.splice(i, 3);
+            i--;
+            break;
+          } else {
+            copyArr[i+1] ? copyArr.splice(i, 2) : copyArr.splice(i, 1);
+            i--;
+            break;
+          }
       case '--discard-prev':
         copyArr[i-1] ? copyArr.splice(i-1, 2) : copyArr.splice(i, 1);
         i < 1 ? i-- : i -= 2;
         break;
       case '--double-next':
-        copyArr[i+1] ? copyArr[i] = copyArr[i+1] : copyArr.splice(i, 1);
-        break;
+          copyArr[i+1] ? copyArr[i] = copyArr[i+1] : copyArr.splice(i, 1);
+          break;
       case '--double-prev':
         copyArr[i-1] ? copyArr[i] = copyArr[i-1] : copyArr.splice(i, 1);
         break;
